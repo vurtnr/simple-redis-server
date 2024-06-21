39cdc431edc1def9905e469f9ff164b05703ec35
yumin.jiao <yumin.jiao@trinasolar.com>
2024-06-21 08:09:00 +0800

    fix: hgetall should return array

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -68,6 +68,7 @@ pub struct HSet {
 #[derive(Debug)]
 pub struct HGetAll {
     key: String,
+    sort: bool,
 }
 
 #[derive(Debug)]
---
+++ src/resp/simple_string.rs
@@ -0,0 +1,94 @@
+use std::ops::Deref;
+
+use bytes::BytesMut;
+
+use crate::{RespDecode, RespEncode, RespError};
+
+use super::{extract_simple_frame_data, CRLF_LEN};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub struct SimpleString(pub(crate) String);
+
+impl SimpleString {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleString(s.into())
+    }
+}
+
+impl RespDecode for SimpleString {
+    const PREFIX: &'static str = "+";
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
+        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
+        // let end = extract_simple_frame_data(buf, "+");
+
+        // split the buffer
+        let data = buf.split_to(end + 2);
+        let s = String::from_utf8_lossy(&data[1..end]);
+        Ok(SimpleString::new(s.to_string()))
+    }
+
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError> {
+        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
+        Ok(end + CRLF_LEN)
+    }
+}
+
+// - simple string: "+OK\r\n"
+impl RespEncode for SimpleString {
+    fn encode(self) -> Vec<u8> {
+        format!("+{}\r\n", self.0).into_bytes()
+    }
+}
+
+impl From<&str> for SimpleString {
+    fn from(s: &str) -> Self {
+        SimpleString(s.to_string())
+    }
+}
+
+impl Deref for SimpleString {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl AsRef<str> for SimpleString {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{RespDecode, RespEncode, RespError, RespFrame, SimpleString};
+    use anyhow::Result;
+    use bytes::{BufMut, BytesMut};
+
+    #[test]
+    fn test_simple_string_encode() {
+        let frame: RespFrame = SimpleString::new("OK".to_string()).into();
+
+        assert_eq!(frame.encode(), b"+OK\r\n");
+    }
+
+    #[test]
+    fn test_simple_string_decode() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b"+OK\r\n");
+
+        let frame = SimpleString::decode(&mut buf)?;
+        assert_eq!(frame, SimpleString::new("OK".to_string()));
+
+        buf.extend_from_slice(b"+hello\r");
+
+        let ret = SimpleString::decode(&mut buf);
+        assert_eq!(ret.unwrap_err(), RespError::NotComplete);
+
+        buf.put_u8(b'\n');
+        let frame = SimpleString::decode(&mut buf)?;
+        assert_eq!(frame, SimpleString::new("hello".to_string()));
+
+        Ok(())
+    }
+}
